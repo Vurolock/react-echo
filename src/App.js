64a34992bfc16c52892e6889575a7dfa07d2b9bb
Andrew Keller <Keller192@gmail.com>
2018-02-02 21:09:22 -0500

    adds cipher import, constructor with this.state, MORE LOGOS, adds functionality to Boxes via onChange, and creates a function to setState the boxes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Box from './Box';
+import cipher from './cipher';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="all-the-logos">
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
           {<h1 className="App-title">React Echo</h1>}
         </header>
         <main>
-          <Box />
+          <div className="put">Input</div>
+          <Box handleOnChange={(this._setText)} />
           <div className="arrow">==></div>
-          <Box />
+          <div className="put">Output 1 (ALL CAPS)</div>
+          <Box content={this.state.content.toUpperCase()} />
+          <div className="arrow">==></div>
+          <div className="put">Output 2 (Ciphered with ROT13)</div>
+          <Box content={cipher(this.state.content, 13)} />
         </main>
       </div>
     );
+  }
+
+  _setText = (text) => {
+    // console.log(text);
+    this.setState({
+      content: text
+    });
   }
 }
 
